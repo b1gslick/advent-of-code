02ba96a57790d01bb8515f2848b1fc797baf8106
Sergei Timokhin <sergei.timokhin@statecraft.tech>
2024-12-03 10:35:15 +0200

    ruby 3 day one star

--- 2024/day_3/lib/day_3.rb
+++ 2024/day_3/lib/day_3.rb
@@ -3,20 +3,44 @@
 # top level
 module DayThree
   def get_number(line)
-    return has_mul(line)
-    [[]]
-  end
-
-  def has_mul(str)
-    prev = str.scan(/mul\(\d+,\d+\)/)
-
+    prev = mul?(line)
     result = []
 
-    for pr in prev do
-      first, second = pr.scan(/\d+,\d+/)
-      result.push([first, second])
+    prev.each do |pr|
+      answer = pr.scan(/\d+,\d+/)
+      numbers = answer[0].split(',')
+      result.push([numbers[0].to_i, numbers[1].to_i])
     end
 
     result
+  end
+
+  def mul?(str)
+    str.scan(/mul\(\d+,\d+\)/)
+  end
+
+  def calculate(list)
+    result = 0
+    list.each do |number|
+      result += (number[0] * number[1])
+    end
+    result
+  end
+
+  def get_after_instruction(line)
+    # print(line =~ /do\(\)/)
+    # print(line =~ /don't\(\)/)
+    #   line.each_byte do |i|
+    #     puts i.chr # Fixnum#chr converts any number to the ASCII char it represents
+    #   end
+    #   oo
+    _test = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    meet_dont = line =~ /don't\(\)/
+    first_part = mul?(line[0..meet_dont])
+    print(first_part)
+    print(line[meet_dont..])
+    first_do = line[meet_dont..line.length] =~ /do\(\)/
+    print("\n")
+    print(line[first_do..line.length])
   end
 end
--- 2024/day_3/spec/day_3.spec.rb
+++ 2024/day_3/spec/day_3.spec.rb
@@ -10,5 +10,25 @@ RSpec.describe 'testing day 3 chalenge' do
       want = [[2, 4], [5, 5], [11, 8], [8, 5]]
       expect(get_number(test_input)).to eq(want)
     end
+
+    it 'should calculate from list' do
+      want = 161
+      test_input = [[2, 4], [5, 5], [11, 8], [8, 5]]
+      expect(calculate(test_input)).to eq(want)
+    end
+
+    it 'it should read info from file and calculate result' do
+      file = open('/Users/sergei.timokhin/Private/advent-of-code/2024/day_3/input.txt')
+      data = file.read
+      number = get_number(data)
+      result = calculate(number)
+      expect(result).to eq(183_669_043)
+    end
+
+    it 'should be read data from memory with instructions' do
+      test_input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+      want = [[2, 4], [8, 5]]
+      expect(get_after_instruction(test_input)).to eq(want)
+    end
   end
 end
