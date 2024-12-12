83b3ff955f4be6c3a8a6cd0c745e59891dc55a9b
Sergei Timokhin <sergei.timokhin@statecraft.tech>
2024-12-12 20:48:10 +0200

    day 12

--- 2024/day_12/day12_test.go
+++ 2024/day_12/day12_test.go
@@ -12,16 +12,39 @@ func Test_day12_read_data(t *testing.T) {
 			t.Errorf("data not equals")
 		}
 	})
+	t.Run("Calculate cost of fence for example 1 should be 140", func(t *testing.T) {
+		got := getCost(findPaths(getData("example.txt")))
+		want := 140
 
-	t.Run("we got struct with area and perimeters", func(t *testing.T) {
-		data := getData("example.txt")
-		got := calculateResult(data)
-		want := []int{4, 4, 10}
-		for index, number := range want {
-			result := got["A"]
-			if result[index] != number {
-				t.Errorf("For A we expected %d, but got %d", want[index], number)
-			}
+		if want != got {
+			t.Errorf("result are not equals got %d, but want %d", got, want)
+		}
+	})
+
+	t.Run("Calculate cost of fence for example 2 should be 772", func(t *testing.T) {
+		got := getCost(findPaths(getData("example2.txt")))
+		want := 772
+
+		if want != got {
+			t.Errorf("result are not equals got %d, but want %d", got, want)
+		}
+	})
+
+	t.Run("Calculate cost of fence for example 3 should be 1930", func(t *testing.T) {
+		got := getCost(findPaths(getData("example3.txt")))
+		want := 1930
+
+		if want != got {
+			t.Errorf("result are not equals got %d, but want %d", got, want)
+		}
+	})
+
+	t.Run("Calculate cost of fence for real input", func(t *testing.T) {
+		got := getCost(findPaths(getData("real.txt")))
+		want := 1402544
+
+		if want != got {
+			t.Errorf("result are not equals got %d, but want %d", got, want)
 		}
 	})
 }
--- 2024/day_12/main.go
+++ 2024/day_12/main.go
@@ -1,10 +1,49 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strings"
 )
+
+type Queue struct {
+	data []tuple
+}
+
+func (q *Queue) Enqueue(elem tuple) {
+	q.data = append(q.data, elem)
+}
+
+func (q *Queue) Dequeue() tuple {
+	if q.IsEmpty() {
+		fmt.Println("Underflow")
+		return tuple{}
+	}
+	tup := q.data[0]
+
+	if q.GetLength() == 1 {
+		q.data = nil
+		return tup
+	}
+	q.data = q.data[1:]
+	return tup
+}
+
+func (q *Queue) GetLength() int {
+	return len(q.data)
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.data) == 0
+}
+
+func (q *Queue) Peek() (tuple, error) {
+	if q.IsEmpty() {
+		return tuple{}, errors.New("empty queue")
+	}
+	return q.data[0], nil
+}
 
 func check(e error) {
 	if e != nil {
@@ -26,31 +65,91 @@ func getData(path string) [][]string {
 	return result
 }
 
-func calculateResult(data [][]string) map[string][]int {
-	var result map[string][]int
+type tuple struct {
+	x int
+	y int
+}
+
+func inArray(pair tuple, array []tuple) bool {
+	for _, tup := range array {
+		if tup.x == pair.x && tup.y == pair.y {
+			return true
+		}
+	}
+	return false
+}
+
+func union(from []tuple, to []tuple) []tuple {
+	result := []tuple{}
+	for _, fr := range from {
+		if inArray(fr, to) && !inArray(fr, result) {
+			result = append(result, fr)
+		}
+	}
+	for _, t := range to {
+		if inArray(t, to) && !inArray(t, result) {
+			result = append(result, t)
+		}
+	}
+	return result
+}
+
+func findPaths(data [][]string) [][]tuple {
+	regions := [][]tuple{}
+	seen := []tuple{}
 
 	row := len(data)
 	cols := len(data[0])
 
 	for i := 0; i < row; i++ {
-		line := data[i]
-
-		for j, area := range line {
-			sides = 4
-
-			value, ok := result[area]
-			if ok {
+		for j := 0; j < cols; j++ {
+			if inArray(tuple{i, j}, seen) {
+				continue
 			}
+			seen = append(seen, tuple{i, j})
+			region := []tuple{{i, j}}
+			q := Queue{}
+			q.Enqueue(tuple{i, j})
+			crop := data[i][j]
+			for !q.IsEmpty() {
+				current := q.Dequeue()
+				for _, tup := range []tuple{{current.x - 1, current.y}, {current.x + 1, current.y}, {current.x, current.y - 1}, {current.x, current.y + 1}} {
+					if tup.x < 0 || tup.y < 0 || tup.x >= row || tup.y >= cols {
+						continue
+					}
+					if data[tup.x][tup.y] != crop {
+						continue
+					}
+					if inArray(tup, region) {
+						continue
+					}
+					region = append(region, tup)
+					q.Enqueue(tup)
+				}
+
+			}
+			region = union(seen, region)
+			regions = append(regions, region)
 		}
 	}
 
+	return regions
+}
+
+func getCost(data [][]tuple) int {
+	result := 0
+	for i := 0; i < len(data); i++ {
+		for _, value := range data[i] {
+			result += 4
+			for _, tup := range []tuple{{value.x - 1, value.y}, {value.x + 1, value.y}, {value.x, value.y - 1}, {value.x, value.y + 1}} {
+				if inArray(tup, data[i]) {
+					result -= 1
+				}
+			}
+		}
+	}
 	return result
 }
 
 func main() {
-	lines := getData("./example.txt")
-
-	for _, line := range lines {
-		fmt.Printf("%s", line)
-	}
 }
