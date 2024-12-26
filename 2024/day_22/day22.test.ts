3d431f272e6dd76e4d15af3f67fc7696a1265ddd
b1gslick <bigslick1106@gmail.com>
2024-12-26 15:30:09 +0200

    day 22 star, by for python, doesn't understand why ts not working

--- 2024/day_22/day22.test.ts
+++ 2024/day_22/day22.test.ts
@@ -1,27 +1,115 @@
-import { calculateGPS, Data, getNewGrid, parseData, readData } from "./day_15";
+import {
+  first_step,
+  makeSequence,
+  second_step,
+  third_step,
+  readData,
+} from "./day_22";
 
 test("should read data from file", () => {
   const has = readData("./example.txt");
-  expect(has.length).toBeGreaterThan(0);
+  expect(has[0]).toEqual(1);
+  expect(has[1]).toEqual(10);
+  expect(has[2]).toEqual(100);
+  expect(has[3]).toEqual(2024);
 });
 
-test("parse steps and fields", () => {
-  const data = readData("./example.txt");
-  const has: Data = parseData(data);
-  expect(has.steps).toEqual(
-    "<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
-  );
+test("should process first step", () => {
+  const has = readData("./example.txt")[0];
+  //@ts-ignore
+  expect(first_step(parseInt(has))).toEqual(65);
 });
 
-test("robot could steps on the grid", () => {
-  const has = parseData(readData("./example.txt"));
-  const grid = getNewGrid(has);
-  const result = calculateGPS(grid);
-  expect(result).toEqual(10092);
+test("second step", () => {
+  const has = readData("./example.txt")[0];
+  //@ts-ignore
+  const after_first = first_step(parseInt(has));
+  expect(second_step(after_first)).toEqual(67);
 });
 
-test("we should result real result", () => {
-  const raw = parseData(readData("./real.txt"));
-  const result = calculateGPS(getNewGrid(raw));
-  expect(result).toEqual(1438161);
+test("third step", () => {
+  const has = readData("./example.txt")[0];
+  //@ts-ignore
+  const after_first = first_step(parseInt(has));
+  const second = second_step(after_first);
+
+  expect(third_step(second)).toEqual(137283);
+});
+
+test("sequence of 123", () => {
+  //@ts-ignore
+  const result = makeSequence(123, 3);
+  expect(result).toEqual(16495136);
+});
+
+type TestData = {
+  times: number;
+  want: number;
+};
+
+test.each<TestData>([
+  {
+    times: 1,
+    want: 15887950,
+  },
+  {
+    times: 2,
+    want: 16495136,
+  },
+  {
+    times: 3,
+    want: 527345,
+  },
+  {
+    times: 4,
+    want: 704524,
+  },
+  {
+    times: 5,
+    want: 1553684,
+  },
+  {
+    times: 6,
+    want: 12683156,
+  },
+  {
+    times: 7,
+    want: 11100544,
+  },
+  {
+    times: 8,
+    want: 12249484,
+  },
+  {
+    times: 9,
+    want: 7753432,
+  },
+  {
+    times: 10,
+    want: 5908254,
+  },
+])("sequence of 123 after $times equal $want", ({ times, want }) => {
+  const result = makeSequence(123, times);
+  //@ts-ignore
+  expect(result).toEqual(want);
+});
+
+test("sequenc of 16495136 should equal to 527345", () => {
+  const result = makeSequence(16495136, 1);
+  //@ts-ignore
+  expect(result).toEqual(527345);
+});
+
+test("sequence 1 of 2000", () => {
+  //@ts-ignore
+  const has = parseInt(readData("./example.txt")[0]);
+  const result = makeSequence(has, 2000);
+  expect(result).toEqual(8685429);
+});
+
+test("sequence 10 of 2000", () => {
+  //@ts-ignore
+  const has = parseInt(readData("./example.txt")[1]);
+  const result = makeSequence(has, 2000);
+  expect(result).toEqual(4700978);
 });
--- 2024/day_22/day_22.ts
+++ 2024/day_22/day_22.ts
@@ -1,116 +1,48 @@
 import * as fs from "fs";
 
-export const readData = (path: string): string => {
+export const readData = (path: string): number[] => {
   const file = fs.readFileSync(path).toString();
-
-  return file;
-};
-
-export type Data = {
-  grid: string[][];
-  steps: string;
-};
-
-export const parseData = (data: string): Data => {
-  const temp = data.split("\n\n");
-  let final_grid = [];
-  let grid = temp[0]?.split("\n") || [];
-  for (let gr of grid) {
-    let t = gr.split("");
-    final_grid.push(t);
-  }
-  let steps = temp[1]?.replaceAll("\n", "").trim() || "";
-  return { grid: final_grid, steps: steps };
-};
-
-export const getStartPosition = (map: string[][]): number[] => {
-  for (let i = 0; i < map.length; i++) {
-    if (!map[i]) {
-      return [0, 0];
-    }
-    let length = map[0]?.length || 0;
-    for (let j = 0; j < length; j++) {
-      const insisde = map[i] || [];
-      if (insisde[j] == "@") {
-        return [i, j];
-      }
-    }
-  }
-  return [0, 0];
-};
-
-const moveV: Record<string, number> = {
-  "^": -1,
-  v: 1,
-};
-
-const moveH: Record<string, number> = {
-  ">": 1,
-  "<": -1,
-};
-
-export const getNewGrid = (data: Data): string[][] => {
-  const start = getStartPosition(data.grid);
-  let grid = data.grid;
-  let r = start[0];
-  let c = start[1];
-  for (let step of data.steps) {
-    let dr = moveV[step] || 0;
-    let dc = moveH[step] || 0;
-    let targets = [start];
-    let cr = r;
-    let cc = c;
-    let go = true;
-    while (true) {
-      //@ts-ignore
-      cr += dr;
-      //@ts-ignore
-      cc += dc;
-      //@ts-ignore
-      let char = grid[cr][cc];
-      if (char == "#") {
-        go = false;
-        break;
-      }
-      if (char == "O") {
-        //@ts-ignore
-        targets.push([cr, cc]);
-      }
-      if (char == ".") {
-        break;
-      }
-    }
-    if (!go) {
-      continue;
-    }
-    //@ts-ignore
-    grid[r][c] = ".";
-    //@ts-ignore
-    grid[r + dr][c + dc] = "@";
-
-    for (const box of targets.slice(1, targets.length)) {
-      //@ts-ignore
-      grid[box[0] + dr][box[1] + dc] = "O";
-    }
-    //@ts-ignore
-    r += dr;
-    //@ts-ignore
-    c += dc;
-  }
-
-  return grid;
-};
-
-export const calculateGPS = (grid: string[][]): number => {
-  let result = 0;
-  for (let i = 0; i < grid.length; i++) {
-    //@ts-ignore
-    for (let j = 0; j < grid[0]?.length; j++) {
-      //@ts-ignore
-      if (grid[i][j] == "O") {
-        result += 100 * i + j;
-      }
+  const result: number[] = [];
+  const temp = file.split("\n");
+  for (let num of temp) {
+    if (num.length > 0) {
+      result.push(parseInt(num));
     }
   }
   return result;
+};
+
+export const makeSequence = (num: number, times: number): number => {
+  let res = num;
+  for (let i = 0; i < times; i++) {
+    res = first_step(res);
+    res = second_step(res);
+    res = third_step(res);
+    console.log(res);
+  }
+
+  return res;
+};
+
+export const first_step = (num: number): number => {
+  const value = num * 64;
+  return prune(mix(value, num));
+};
+
+export const second_step = (num: number): number => {
+  const value = num / 32;
+  return prune(mix(value | 0, num));
+};
+
+export const third_step = (num: number): number => {
+  const value = num * 2048;
+  return prune(mix(value, num));
+};
+
+export const mix = (values: number, secret: number): number => {
+  return values ^ secret;
+};
+
+export const prune = (secret: number): number => {
+  return secret % 16777216;
 };
